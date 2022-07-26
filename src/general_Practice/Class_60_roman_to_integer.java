ed7a9d94c7f6bbbf99d63ecc712f400abd6e3b99
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2022-07-26 13:16:26 +0530

    Roman to Integer

---
+++ src/general_Practice/Class_60_roman_to_integer.java
@@ -0,0 +1,34 @@
+package general_Practice;
+import java.util.*;
+
+public class Class_60_roman_to_integer {
+	public static void main(String[] args) {
+		String roman = "CDXLIII";
+		int ans = romanToInt("CDXLIII");
+		System.out.println(ans);
+	}
+	 public static int romanToInt(String s) {
+	        HashMap<Character, Integer> map = new HashMap<>();
+	        map.put('I', 1);
+	        map.put('V', 5);
+	        map.put('X', 10);
+	        map.put('L', 50);
+	        map.put('C', 100);
+	        map.put('D', 500);
+	        map.put('M', 1000);
+	        
+	        int sum = 0;
+	        char lastChar = s.charAt(s.length() -1);
+	        sum = sum + map.get(lastChar);
+	        
+	        for(int i = s.length() -2; i >= 0; i--){
+	            if(map.get(s.charAt(i)) >= map.get(s.charAt(i+1))){
+	                sum = sum + map.get(s.charAt(i));
+	            }else{
+	                sum = sum - map.get(s.charAt(i));
+	            }
+	        }
+	        
+	        return sum;
+	    }
+}
--- src/general_Practice/Class_Practice.java
+++ src/general_Practice/Class_Practice.java
@@ -13,7 +13,9 @@ public class Class_Practice {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		for(int i = 0; i < 2; i++) {
+			System.out.println(i);
+		}
 	
 		
 	}
